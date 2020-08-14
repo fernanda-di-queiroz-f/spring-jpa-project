e7f667ee989a2c23e08ac1987a75bb9d4b36253f
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-08-14 11:05:36 -0300

    Project Created

---
+++ src/test/java/com/fernandapackage/springjpaproject/SpringjpaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandapackage.springjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
