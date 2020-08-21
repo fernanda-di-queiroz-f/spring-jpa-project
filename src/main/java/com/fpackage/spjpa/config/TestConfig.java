6fc75b1d2f735c94b6e9ad389bfe0053c666dffe
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-08-21 11:59:43 -0300

    Payment, one-to-one association

--- src/main/java/com/fpackage/spjpa/config/TestConfig.java
+++ src/main/java/com/fpackage/spjpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fpackage.spjpa.entities.Category;
 import com.fpackage.spjpa.entities.Order;
 import com.fpackage.spjpa.entities.OrderItem;
+import com.fpackage.spjpa.entities.Payment;
 import com.fpackage.spjpa.entities.Product;
 import com.fpackage.spjpa.entities.User;
 import com.fpackage.spjpa.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
