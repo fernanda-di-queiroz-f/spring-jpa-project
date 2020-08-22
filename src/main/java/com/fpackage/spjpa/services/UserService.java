43092c4777ceb4c446f8705e2acb9b35b8bbc8a9
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-08-22 14:55:58 -0300

    User delete

--- src/main/java/com/fpackage/spjpa/services/UserService.java
+++ src/main/java/com/fpackage/spjpa/services/UserService.java
@@ -23,9 +23,13 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
+	}
+
+	public void delete(Long id) {
+		userRepository.deleteById(id);
 	}
 
 }
