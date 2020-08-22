79ff85f333e985a19316062c9561edad656d6e15
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-08-22 18:45:19 -0300

    Exception handling - findById

--- src/main/java/com/fpackage/spjpa/services/UserService.java
+++ src/main/java/com/fpackage/spjpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.fpackage.spjpa.entities.User;
 import com.fpackage.spjpa.repositories.UserRepository;
+import com.fpackage.spjpa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
