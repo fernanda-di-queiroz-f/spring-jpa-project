0659cb8d03621f2031cb7567799d09073126a4bd
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-08-24 20:00:18 -0300

    Dev profile

--- src/test/java/com/fpackage/spjpa/JpaApplicationTests.java
+++ src/test/java/com/fpackage/spjpa/JpaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fernandapackage.springjpaproject;
+package com.fpackage.spjpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringjpaprojectApplicationTests {
+class JpaApplicationTests {
 
 	@Test
 	void contextLoads() {
